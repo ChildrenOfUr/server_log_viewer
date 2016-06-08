a21efbc3de212320bc72f9f9bfbf15141fa0a556
Andy Castille <klikini@robiotic.net>
2016-06-07 19:40:12 -0500

    more reliable collapse detection

--- js/collapse.js
+++ js/collapse.js
@@ -1,9 +1,19 @@
+var lastWidth = -1;
+
 function updateLines() {
+	var singleLineSize = $('#sizing').height();
+
+	if (lastWidth !== -1 && lastWidth === $(window).width()) {
+		// No horizontal size change
+		return;
+	}
+
 	$('pre').each((function() {
 		// Remove old listeners
 		$(this).off('click');
+		$(this).attr('closed', 'false');
 
-		if ($(this).innerHeight() > 16) {
+		if ($(this).innerHeight() > singleLineSize) {
 			// Multiple lines
 			$(this).addClass('collapse');
 			$(this).attr('closed', 'true');
@@ -20,9 +30,12 @@ function updateLines() {
 			$(this).attr('closed', 'false');
 		}
 	}));
+
+	lastWidth = $(window).width();
 }
 
 $(document).ready(function() {
+	$('body').append('<pre id="sizing">&nbsp</pre>');
 	updateLines();
 });
 
